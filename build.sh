153b2df5db6ca04a1901c34ee3b57e72614666a1
Aaron Griffith <aargri@gmail.com>
2025-12-14 19:59:20 -0500

    make mrproper in u-boot

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ cp configs/u-boot/*.dts{,i} sources/u-boot/arch/arm/dts/
 
 (
     cd sources/u-boot/
+    make mrproper
     KCFLAGS=-Wno-error ./make.sh CROSS_COMPILE=arm-none-eabi- rk3506_luckfox
 )
 
