ca840c024ba117dce1e0839217677170512b7172
Aaron Griffith <aargri@gmail.com>
2025-12-15 15:39:59 -0500

    make u-boot.itb ourselves, use boot_merger directly

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/bash
 set -e
-cd "$(dirname "$0")"
+R=$(realpath $(dirname "$0"))
+cd $R
+
+JOBS=${JOBS:=$(nproc --all)}
+mkdir -p build/parts/
+
+#
+# idblock
+#
+
+(
+    cd sources/rkbin/
+
+    ./tools/boot_merger RKBOOT/RK3506MINIALL.ini
+    rm rk3506_spl_loader_v1.06.111.bin
+    mv rk3506_idblock_v1.06.111.img $R/build/parts/idblock.img
+)
 
 #
 # U-Boot
@@ -11,13 +27,25 @@ cp configs/u-boot/*.dts{,i} sources/u-boot/arch/arm/dts/
 
 (
     cd sources/u-boot/
+
+    # build u-boot
     make mrproper
-    KCFLAGS=-Wno-error ./make.sh CROSS_COMPILE=arm-none-eabi- rk3506_luckfox
+    make rk3506_luckfox_defconfig
+    make -j${JOBS} CROSS_COMPILE=arm-none-eabi- KCFLAGS=-Wno-error
+
+    # grab op-tee from rkbin, and create the u-boot FIT
+    cp ../rkbin/bin/rk35/rk3506_tee_v2.10.bin ./tee.bin
+    make -j${JOBS} CROSS_COMPILE=arm-none-eabi- KCFLAGS=-Wno-error u-boot.itb
+
+    # fix up op-tee load address
+    fdtput -t x u-boot.itb /images/optee entry 0x1000
+    fdtput -t x u-boot.itb /images/optee load 0x1000
+
+    mv u-boot.itb $R/build/parts/
 )
 
 #
 # Debos
 #
 
-mkdir -p build/
 debos --artifactdir=build/ debos.yaml
