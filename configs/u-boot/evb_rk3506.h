5cc09082d91d25960b7647a3dc18645ff27474c6
Aaron Griffith <aargri@gmail.com>
2025-12-18 17:41:12 -0500

    move dt address to give it more space, turn on dt symbols

--- configs/u-boot/evb_rk3506.h
+++ configs/u-boot/evb_rk3506.h
@@ -24,4 +24,23 @@
 #define CONFIG_BOOTCOMMAND RKIMG_BOOTCOMMAND */
 #endif
 
+/*
+ * rearrange addresses to give fdt some more room
+ *
+ *   (trust):    0K - 392K
+ * (ramoops):  524K - 704K
+ *     Image:  1M+32k - 17M
+ *    zImage:  17M - 24M
+ *       fdt:  24M - 25M
+ *   ramdisk:  25M - ...
+ */
+#undef ENV_MEM_LAYOUT_SETTINGS
+#define ENV_MEM_LAYOUT_SETTINGS \
+        "scriptaddr=0x00b00000\0"       \
+        "pxefile_addr_r=0x00c00000\0"   \
+        "kernel_addr_r=0x00108000\0"    \
+        "kernel_addr_c=0x01100000\0"    \
+        "fdt_addr_r=0x01800000\0"       \
+        "ramdisk_addr_r=0x01900000\0"
+
 #endif /* __CONFIGS_RK3506_EVB_H */
