e069a8c2f33d9b08b7b13fae6a94f0a520114748
Aaron Griffith <aargri@gmail.com>
2025-12-18 16:10:56 -0500

    add an initramfs

--- scripts/configure-boot-menu.sh
+++ scripts/configure-boot-menu.sh
@@ -7,7 +7,6 @@ mkdir -p /etc/u-boot-menu/conf.d/
 
 cat > /etc/u-boot-menu/conf.d/50-lyra.conf <<EOF
 U_BOOT_PARAMETERS="earlycon=uart8250,mmio32,0xff0a0000 console=tty1 console=ttyS0,1500000 rootwait ro"
-U_BOOT_ROOT="root=/dev/mmcblk0p4"
 U_BOOT_TIMEOUT="20"
 U_BOOT_FDT="$DEVICETREE"
 U_BOOT_SYNC_DTBS="true"
