1eb14442c4bf90bd80bb509def11345a478b78e3
Aaron Griffith <aargri@gmail.com>
2025-12-18 12:29:04 -0500

    move u-boot-menu config to script so we can set dt path

--- scripts/configure-boot-menu.sh
+++ scripts/configure-boot-menu.sh
@@ -1,5 +1,14 @@
+#!/bin/sh
+
+DEVICETREE=$1
+[ "$DEVICETREE" ] || exit 1
+
+mkdir -p /etc/u-boot-menu/conf.d/
+
+cat > /etc/u-boot-menu/conf.d/50-lyra.conf <<EOF
 U_BOOT_PARAMETERS="earlycon=uart8250,mmio32,0xff0a0000 console=tty1 console=ttyS0,1500000 rootwait ro"
 U_BOOT_ROOT="root=/dev/mmcblk0p4"
 U_BOOT_TIMEOUT="20"
-U_BOOT_FDT="rk3506g-luckfox-lyra.dtb"
+U_BOOT_FDT="$DEVICETREE"
 U_BOOT_SYNC_DTBS="true"
+EOF
